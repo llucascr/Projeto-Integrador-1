22ac902c08969ec9bda6fe7ac525c05ac7da568e
PucLucasCampos <lucas.cr16@puccampinas.edu.br>
2024-04-23 11:27:25 -0300

    main.py: add tabela dos produtos com os calculos  |  Add: jhe.py e TESTE.py

--- conexao.py
+++ conexao.py
@@ -12,12 +12,65 @@ cursor = connection.cursor()
 # [1] MOSTRAR ESTOQUE
 def mostrar_estoque():
     for row in cursor.execute("SELECT * FROM PRODUTOS"):
-                print(f'''
-                      
-CÓDIGO PRODUTO: {row[0]}
-PRODUTO: {row[1]}
-DESCRIÇÃO: {row[2]}
-''')
+        estoque = row
+        print(estoque)
+     # Preço de Venda
+        pv = estoque[3] / (1 - ((estoque[4] + estoque[5] + estoque[6] + estoque[7]) / 100))
+
+        # ---------------------------- CALCULOS DOS VALORES -----------------------------
+        # 'porc' = porcentagem
+        # 'valor' = variaveis float
+        porc_aquisicao = (estoque[3] * 100) / pv                            # LINHA B
+
+        receita_bruta = pv - estoque[3]                                     # LINHA C
+        valor_rb = pv - estoque[3]
+        porc_c = 100 - porc_aquisicao
+
+        valor_cf = (pv * estoque[4]) / 100       #cf                            # LINHA D
+        valor_cv = (pv * estoque[5]) / 100       #cv                            # LINHA E
+        valor_iv = (pv * estoque[6]) / 100       #iv                            # LINHA F
+
+        outros = valor_cf + valor_cv + valor_iv                           # LINHA G
+        porc_outros = estoque[4] + estoque[5] + estoque[6]
+        
+        valor_rt = valor_rb - outros                                      # LINHA H
+        # Porcentual de Rentabilidade
+        porc_rt = porc_c - porc_outros
+        # ---------------------------- CLASSIFICAÇÃO DE LUCRO -----------------------------
+        if porc_rt > 20:
+            class_lucro = "Alto"
+        elif 10 <= porc_rt <= 20:
+            class_lucro = "Médio"
+        elif 0 < porc_rt < 10:
+            class_lucro = "Baixo"
+        elif porc_rt == 0:
+            class_lucro = "Equilibrio"
+        else:
+            class_lucro = "Prejuizo"
+
+        print(f'''
+
+CÓDIGO PRODUTO: {estoque[0]}
+PRODUTO: {estoque[1]}
+DESCRIÇÃO: {estoque[2]}
+
+==========================================================================================================
+        DESCRIÇÃO               |\t\t\tVALOR\t\t\t \t\t%
+
+PREÇO DE VENDA                  |\t\t\tR${pv:.2f}\t\t\t \t\t100.00%
+CUSTO DE AQUISIÇÃO              |\t\t\tR${estoque[3]:.2f}\t\t\t \t\t{porc_aquisicao:.2f}%
+RECEITA BRUTA                   |\t\t\tR${valor_rb:.2f}\t\t\t \t\t{porc_c:.2f}%
+CUSTO FIXO/ADMINISTRATIVO       |\t\t\tR${valor_cf:.2f}\t\t\t \t\t{estoque[4]:.2f}%
+COMISSÃO DE VENDAS              |\t\t\tR${valor_cv:.2f}\t\t\t \t\t{estoque[5]:.2f}%
+IMPOSTOS                        |\t\t\tR${valor_iv:.2f}\t\t\t \t\t{estoque[6]:.2f}%
+OUTROS CUSTOS                   |\t\t\tR${outros:.2f}\t\t\t \t\t{porc_outros:.2f}%
+RENTABILIDADE                   |\t\t\tR${valor_rt:.2f}\t\t\t \t\t{porc_rt:.2f}%
+
+CLASSIFICAÇÃO DE LUCRO:        {class_lucro}
+==========================================================================================================
+
+        ''')
+
 
 # [2] CRIAR TABELA  
 def criar_tabela():
--- main.py
+++ main.py
@@ -18,27 +18,10 @@ menu = int(input("""
         [6] DELETAR PRODUTO
         [0] SAIR DO PROGRAMA
         """))
-while menu > 0:
+while menu != 0:
 # ---------------------------- ROTINA DE PRODUTOS -----------------------------
     if menu == 1:
         conexao.mostrar_estoque()
-        print(f"""
-==========================================================================================================
-        DESCRIÇÃO               |\t\t\tVALOR\t\t\t \t\t%
-
-PREÇO DE VENDA                  |\t\t\tR${pv:.2f}\t\t\t \t\t100.00%
-CUSTO DE AQUISIÇÃO              |\t\t\tR${valor_cp:.2f}\t\t\t \t\t{porc_aquisicao:.2f}%
-RECEITA BRUTA                   |\t\t\tR${valor_rb:.2f}\t\t\t \t\t{porc_c:.2f}%
-CUSTO FIXO/ADMINISTRATIVO       |\t\t\tR${valor_cf:.2f}\t\t\t \t\t{porc_cf:.2f}%
-COMISSÃO DE VENDAS              |\t\t\tR${valor_cv:.2f}\t\t\t \t\t{porc_cv:.2f}%
-IMPOSTOS                        |\t\t\tR${valor_iv:.2f}\t\t\t \t\t{porc_iv:.2f}%
-OUTROS CUSTOS                   |\t\t\tR${outros:.2f}\t\t\t \t\t{porc_outros:.2f}%
-RENTABILIDADE                   |\t\t\tR${valor_rt:.2f}\t\t\t \t\t{porc_rt:.2f}%
-
-CLASSIFICAÇÃO DE LUCRO:        {class_lucro}
-==========================================================================================================
-
-        """)
     elif menu == 2:
         conexao.criar_tabela()
         print("Tabela Criada com Sucesso")
@@ -67,66 +50,7 @@ CLASSIFICAÇÃO DE LUCRO:        {class_lucro}
         porc_cv = float(input("Comissão de Vendas (CV): "))
         porc_iv = float(input("Impostos (IV): "))
         porc_ml = float(input("Rentabilidade (ML): "))
-
-        # Preço de Venda
-        pv = valor_cp / (1 - ((porc_cf + porc_cv + porc_iv + porc_ml) / 100))
-
-        # ---------------------------- CALCULOS DOS VALORES -----------------------------
-        # 'porc' = porcentagem
-        # 'valor' = variaveis float
-        porc_aquisicao = (valor_cp * 100) / pv                            # LINHA B
-
-        receita_bruta = pv - valor_cp                                     # LINHA C
-        valor_rb = pv - valor_cp
-        porc_c = 100 - porc_aquisicao
-
-        valor_cf = (pv * porc_cf) / 100                                   # LINHA D
-        valor_cv = (pv * porc_cv) / 100                                   # LINHA E
-        valor_iv = (pv * porc_iv) / 100                                   # LINHA F
-
-        outros = valor_cf + valor_cv + valor_iv                           # LINHA G
-        porc_outros = porc_cf + porc_cv + porc_iv
-
-        valor_rt = valor_rb - outros                                      # LINHA H
-        # Porcentual de Rentabilidade
-        porc_rt = porc_c - porc_outros
-
-        # ---------------------------- CLASSIFICAÇÃO DE LUCRO -----------------------------
-        if porc_rt > 20:
-            class_lucro = "Alto"
-        elif 10 <= porc_rt <= 20:
-            class_lucro = "Médio"
-        elif 0 < porc_rt < 10:
-            class_lucro = "Baixo"
-        elif porc_rt == 0:
-            class_lucro = "Equilibrio"
-        else:
-            class_lucro = "Prejuizo"
-        # ---------------------------- SAIDA DE VALORES -------------------------------
         os.system('cls')
-        print(f'''
-
-CÓDIGO PRODUTO: {codigo_produto}
-PRODUTO: {nome_produto}
-DESCRIÇÃO: {descricao_produto}
-
-==========================================================================================================
-        DESCRIÇÃO               |\t\t\tVALOR\t\t\t \t\t%
-
-PREÇO DE VENDA                  |\t\t\tR${pv:.2f}\t\t\t \t\t100.00%
-CUSTO DE AQUISIÇÃO              |\t\t\tR${valor_cp:.2f}\t\t\t \t\t{porc_aquisicao:.2f}%
-RECEITA BRUTA                   |\t\t\tR${valor_rb:.2f}\t\t\t \t\t{porc_c:.2f}%
-CUSTO FIXO/ADMINISTRATIVO       |\t\t\tR${valor_cf:.2f}\t\t\t \t\t{porc_cf:.2f}%
-COMISSÃO DE VENDAS              |\t\t\tR${valor_cv:.2f}\t\t\t \t\t{porc_cv:.2f}%
-IMPOSTOS                        |\t\t\tR${valor_iv:.2f}\t\t\t \t\t{porc_iv:.2f}%
-OUTROS CUSTOS                   |\t\t\tR${outros:.2f}\t\t\t \t\t{porc_outros:.2f}%
-RENTABILIDADE                   |\t\t\tR${valor_rt:.2f}\t\t\t \t\t{porc_rt:.2f}%
-
-CLASSIFICAÇÃO DE LUCRO:        {class_lucro}
-==========================================================================================================
-
-        ''')
-
         # conexao.add_produto()
     elif menu == 4:
         nova_coluna = str(input("Nome e tipo da coluna: ")).upper
@@ -146,6 +70,5 @@ CLASSIFICAÇÃO DE LUCRO:        {class_lucro}
         [0] SAIR DO PROGRAMA
         """))
 
-connection.commit() # Salva as informações
 cursor.close() # Encerra o cursor 
 connection.close()
