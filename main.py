8ce7fe2326dc68f8d87bac7b344b315eb5a01a94
Jhenifer Laís Barbosa <161936678+Jheniferlb21@users.noreply.github.com>
2024-04-26 14:34:41 -0300

    "oficial" main.py
    versao "oficial", sem coisas desnecessarias

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ while menu != 0:
     elif menu == 3: #CADASTRAR PRODUTO
         # ---------------------------- CADASTRO DOS PRODUTOS ----------------------------
         cod_prod = int(input('Código do Produto: '))       #CODIGO DO PRODUTO
-        nome_prod = str(input('Nome Produto: '))              #NOME DO PRODUTO
-        desc_prod = str(input('Descrição do Produto: ')) #DESCRIÇÃO DO PRODUTO
+        nome_prod = str(input('Nome Produto: '))           #NOME DO PRODUTO
+        desc_prod = str(input('Descrição do Produto: '))   #DESCRIÇÃO DO PRODUTO
 
         CP = float(input('Custo do  Produto: '))              #CUSTO PAGO PELO PRODUTO PARA O FORNECEDOR
         ML = float(input('Margem de Lucro sobre a Venda: '))  #MARGEM DE LUCRO SOBRE A VENDA DO PRODUTO
-        CF = float(input('Custo Fixo/Administrativo(%): '))    #CUSTO FIXO (ESPAÇO FÍSICO, DESPESAS, FUNCIONÁRIOS...)
+        CF = float(input('Custo Fixo/Administrativo(%): '))   #CUSTO FIXO (ESPAÇO FÍSICO, DESPESAS, FUNCIONÁRIOS...)
         CV = float(input('Comissão de Vendas(%): '))          #COMISSÃO SOBRE A VENDA DO PRODUTO
         IV = float(input('Impostos(%): '))                    #IMPOSTOS SOBRE A VENDA DO PRODUTO
 
@@ -51,18 +51,19 @@ while menu != 0:
 
         cursor.execute("INSERT INTO produtos VALUES (:1, :2, :3, :4, :5, :6, :7, :8)",(cod_prod, nome_prod, desc_prod, CP, CF, CV, IV, ML))
         connection.commit()
+        
+        conexao.mostrar_estoque()
+
         print("""
-                    PRODUTO CADASTRADO COM SUCESSO!!!""")
-        #conexao.add_produto()
-    elif menu == 4: #DELETAR TABELA
+                    PRODUTO CADASTRADO COM SUCESSO!!!
+              """)
+    elif menu == 4: #APAGAR TABELA
         conexao.deletar_tabela()
-    elif menu == 5: #DELETAR PRODUTO
+    elif menu == 5: #DELETAR TODOS PRODUTOS
         conexao.deletar_produto()
     
     menu = int(input("""
-=================================================================
-                          BEM VINDO AO
-                  SISTEMA DE CADASTRO DE PRODUTO!!!
+
 =================================================================
 OPÇÕES:
 [1].MOSTRAR PRODUTO/ESTOQUE
